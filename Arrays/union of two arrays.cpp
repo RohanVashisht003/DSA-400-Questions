c8a79d9b8dc9110b3aa12f286b87090df978614a
Rohan Vashisht <38130791+RohanVashisht003@users.noreply.github.com>
2020-11-14 13:17:04 +0530

    sixth commit

---
+++ Arrays/union of two arrays.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<set>
+using namespace std;
+
+int main()
+{
+    int t;
+    int arr[100];
+    cin>>t;
+    while(t--)
+    {
+        int n,m;
+        cin>>n>>m;
+        int k=0;
+        set<int>set1;
+        for(int i=0; i<n; i++)
+        {
+            cin>>k;
+            set1.insert(k);
+        }
+
+
+        for(int i=0; i<m; i++)
+        {
+            cin>>k;
+            set1.insert(k);
+        }
+        cout<<set1.size()<<endl;
+    }
+}
